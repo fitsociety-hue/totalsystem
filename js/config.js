b2b4e545150c823407c70bfaba10a3c469bae105
effect082 <your-email@example.com>
2026-07-22 14:20:46 +0900

    chore: GAS 웹앱 신규 배포 URL 반영

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // Google Apps Script 웹앱 배포 URL
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzMOgzeMFM0f1GO44VHw75YtBAZgJVw-4nua76chAj3Nvuq7oavn1bl6BArT-Gd1Mho/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbxf9CP2RADrObyUAOtHS3hPlBS45Jkk0liNHTKJCo7HhnuojKfhJEQ-fy8LzaVDX656/exec',
   APP_NAME: '강동어울림복지관 출석부',
   VERSION: '1.0.0'
 };
