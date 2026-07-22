1fca9db288ad2adaed3e220b3c1b9774c704faf2
effect082 <your-email@example.com>
2026-07-22 15:06:05 +0900

    fix: update GAS deployment URL and update static pre-rendered snapshot data

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   // Google Apps Script 웹앱 배포 URL
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbxf9CP2RADrObyUAOtHS3hPlBS45Jkk0liNHTKJCo7HhnuojKfhJEQ-fy8LzaVDX656/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwzsvUQxHC0FULpZOvJCzYEi0wPz4nx7Ed8oo-W7Ayu1p46F2s2n7gLVTsdWsTA3p-J/exec',
   APP_NAME: '강동어울림복지관 출석부',
   VERSION: '1.0.0'
 };
